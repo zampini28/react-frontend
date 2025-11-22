86148f6b6264dbb0b57fa3a804d2e0210e87b1f9
user <user@localhost>
2025-11-21 22:42:38 -0300

    minor fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
 
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+
     </HashRouter>
   </React.StrictMode>,
 )
